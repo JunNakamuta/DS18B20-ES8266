6ffdef3fcc647992a3e11fcc14d5fa85e879757b
Jun Nakamuta <jun@nakamuta.biz>
2021-09-20 20:41:37 +0900

    first commit

--- boot.py
+++ boot.py
@@ -1,10 +1,9 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
-# esp.osdebug(None)
-import uos
-import machine
-# uos.dupterm(None, 1) # disable REPL on UART(0)
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 #import webrepl
-# webrepl.start()
+#webrepl.start()
 gc.collect()
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import ssd1306
+from machine import Pin, I2C
 import time
 import ds18x20
-from machine import Pin
 import onewire
+
+# SSD1306 OLED display
+
+i2c = I2C(sda=Pin(4), scl=Pin(5))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+display.text('Hello, World!', 0, 0, 1)
+display.show()
+
 
 ow = onewire.OneWire(Pin(0))  # GPIO5 で OneWire バスを作成
 ds = ds18x20.DS18X20(ow)
@@ -16,8 +26,7 @@ while(True):
     ds.convert_temp()
     time.sleep_ms(750)
     for rom in roms:
-        print('Temp:[%03.01f]C ' % ds.read_temp(rom), end='  ')
-    print('')
+        print('Temp:[%03.02f]C ' % ds.read_temp(rom), end='\n')
 
     led = Pin(13, Pin.OUT)
     led.on()
